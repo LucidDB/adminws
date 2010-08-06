e710372e13389fd79b48bded8d604225fbc59b38
Kevin Secretan <ksecretan@dynamobi.com>
2010-08-06 02:12:56 +0000

    Added license headers, fixes DWS-14

--- src/com/dynamobi/ws/domain/Column.java
+++ src/com/dynamobi/ws/domain/Column.java
@@ -1,3 +1,21 @@
+/*
+Dynamo Web Services is a web service project for administering LucidDB
+Copyright (C) 2010 Dynamo Business Intelligence Corporation
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option)
+any later version approved by Dynamo Business Intelligence Corporation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 package com.dynamobi.ws.domain;
 
 import java.util.Date;
